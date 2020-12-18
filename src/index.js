854b63f1168a032b18d3047b36dc3b6e6e849de8
gauravnumber <gauravnumber@gmail.com>
2020-12-18 15:52:56 +0530

    adding webpack in development mode

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const api_key = "808d67d7b2bd95c69791f456b64b9e0e";
+
+async function f() {
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=${ api_key }&units=metric`)
+    const data = await response.json();
+
+    let date = new Date(data.sys.sunrise * 1000)
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+    let secs = date.getSeconds();
+    let formattedDate = `${hours}:${mins}:${secs}`;
+
+    console.log(`Current temperature: ${data.main.temp}`)
+    console.log(`Location: ${data.name}`)
+    console.log(`Sunrise: ${data.sys.sunrise}`)
+    console.log(`Sunrise: ${formattedDate}`)
+}
+
+f();
