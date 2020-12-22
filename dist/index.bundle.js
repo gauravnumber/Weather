21985e7461d17cec8ee81124ebf09b6963e5f7f4
gauravnumber <gauravnumber@gmail.com>
2020-12-22 12:59:09 +0530

    this is not work as expected

--- dist/index.bundle.js
+++ dist/index.bundle.js
@@ -2,6 +2,6 @@
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-throw new Error("Module parse failed: Cannot use keyword 'await' outside an async function (10:25)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|         document.getElementById('lon').innerHTML = position.coords.longitude;\n| \n>         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${ api_key }&units=metric`);\n|         const data = await response.json();\n| ");
+throw new Error("Module parse failed: Unexpected token (91:8)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|                 */\n|             }\n>         }\n| f();");
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -3,48 +3,90 @@ let lat, lon;
 
 async function f() {
 
-    navigator.geolocation.getCurrentPosition((position) => {
-        document.getElementById('lat').innerHTML = position.coords.latitude;
-        document.getElementById('lon').innerHTML = position.coords.longitude;
+        navigator.geolocation.getCurrentPosition((position) => {
+                document.getElementById('lat').innerHTML = position.coords.latitude;
+                document.getElementById('lon').innerHTML = position.coords.longitude;
 
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${ api_key }&units=metric`);
-        const data = await response.json();
+                fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${ api_key }&units=metric`)
+                    .then(response => response.json())
+                    .then(data => {
+                        let date = new Date(data.sys.sunrise * 1000)
+                        let hours = date.getHours();
+                        let mins = date.getMinutes();
+                        let secs = date.getSeconds();
 
-        let date = new Date(data.sys.sunrise * 1000)
-        let hours = date.getHours();
-        let mins = date.getMinutes();
-        let secs = date.getSeconds();
+                        if (hours < 10) {
+                            hours = '0' + hours;
+                        }
 
-        if (hours < 10) {
-            hours = '0' + hours;
+                        if (mins < 10) {
+                            mins = '0' + mins;
+                        }
+
+                        if (secs < 10) {
+                            secs = '0' + secs;
+                        }
+
+                        let formattedDate = `${hours}:${mins}:${secs}`;
+
+                        let sunset = new Date(data.sys.sunset * 1000)
+                        let sunsetHours = sunset.getHours();
+                        let sunsetMins = sunset.getMinutes();
+                        let sunsetSecs = sunset.getSeconds();
+                        let sunsetDate = `${sunsetHours}:${sunsetMins}:${sunsetSecs}`;
+
+
+                        document.getElementById('name').innerHTML = data.name;
+                        document.getElementById('temp').innerHTML = data.main.temp;
+                        document.getElementById('min').innerHTML = data.main.temp_min;
+                        document.getElementById('max').innerHTML = data.main.temp_max;
+                        document.getElementById('like').innerHTML = data.main.feels_like;
+                        document.getElementById('sunrise').innerHTML = formattedDate;
+                        document.getElementById('sunset').innerHTML = sunsetDate;
+
+                });
+
+
+
+                /*
+                        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${ api_key }&units=metric`);
+                        const data = await response.json();
+
+                        let date = new Date(data.sys.sunrise * 1000)
+                        let hours = date.getHours();
+                        let mins = date.getMinutes();
+                        let secs = date.getSeconds();
+
+                        if (hours < 10) {
+                            hours = '0' + hours;
+                        }
+
+                        if (mins < 10) {
+                            mins = '0' + mins;
+                        }
+
+                        if (secs < 10) {
+                            secs = '0' + secs;
+                        }
+
+                        let formattedDate = `${hours}:${mins}:${secs}`;
+
+                        let sunset = new Date(data.sys.sunset * 1000)
+                        let sunsetHours = sunset.getHours();
+                        let sunsetMins = sunset.getMinutes();
+                        let sunsetSecs = sunset.getSeconds();
+                        let sunsetDate = `${sunsetHours}:${sunsetMins}:${sunsetSecs}`;
+
+
+                        document.getElementById('name').innerHTML = data.name;
+                        document.getElementById('temp').innerHTML = data.main.temp;
+                        document.getElementById('min').innerHTML = data.main.temp_min;
+                        document.getElementById('max').innerHTML = data.main.temp_max;
+                        document.getElementById('like').innerHTML = data.main.feels_like;
+                        document.getElementById('sunrise').innerHTML = formattedDate;
+                        document.getElementById('sunset').innerHTML = sunsetDate;
+                    });
+                */
+            }
         }
-
-        if (mins < 10) {
-            mins = '0' + mins;
-        }
-
-        if (secs < 10) {
-            secs = '0' + secs;
-        }
-
-        let formattedDate = `${hours}:${mins}:${secs}`;
-
-        let sunset = new Date(data.sys.sunset * 1000)
-        let sunsetHours = sunset.getHours();
-        let sunsetMins = sunset.getMinutes();
-        let sunsetSecs = sunset.getSeconds();
-        let sunsetDate = `${sunsetHours}:${sunsetMins}:${sunsetSecs}`;
-
-
-        document.getElementById('name').innerHTML = data.name;
-        document.getElementById('temp').innerHTML = data.main.temp;
-        document.getElementById('min').innerHTML = data.main.temp_min;
-        document.getElementById('max').innerHTML = data.main.temp_max;
-        document.getElementById('like').innerHTML = data.main.feels_like;
-        document.getElementById('sunrise').innerHTML = formattedDate;
-        document.getElementById('sunset').innerHTML = sunsetDate;
-    });
-
-}
-
 f();
